cba9613b5d4922a3aee8d69f11c8f662fe11c4bf
ZynXx <techigwe@gmail.com>
2025-09-16 22:57:54 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,6 @@
+HEADER = { "User-Agent": "mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko)"
+                  " Chrome/116.0.0.0 Safari/537.36"
+}
+
+BASE_URL = "https://crazy.com"  # met le lien du site que tu veux scrappe ici frr
+REQUEST_TIMEOUT = 10  # secondes
