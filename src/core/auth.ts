da58de46075912f5eef552c1ada58bd60210fb4a
ZynXx <techigwe@gmail.com>
2026-02-27 18:13:03 +0100

    Create auth.ts

---
+++ src/core/auth.ts
@@ -0,0 +1,17 @@
+import { supabase } from "./supabase";
+
+export async function signIn(email: string, password: string) {
+  return supabase.auth.signInWithPassword({ email, password });
+}
+
+export async function signUp(email: string, password: string) {
+  return supabase.auth.signUp({ email, password });
+}
+
+export async function getUser() {
+  return supabase.auth.getUser();
+}
+
+export async function signOut() {
+  return supabase.auth.signOut();
+}
